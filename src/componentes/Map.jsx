e000f219a11fd1e6cbe0ea338f8d28ce3dfe2fb2
Fayukakm <137416147+Fayukakm@users.noreply.github.com>
2024-03-01 20:06:21 -0600

    Update Map.jsx

--- src/componentes/Map.jsx
+++ src/componentes/Map.jsx
@@ -2,32 +2,42 @@ import React, { useEffect } from 'react';
 
 function Map() {
   useEffect(() => {
-    // Función para inicializar el mapa con la API de Google Maps
     const initMap = () => {
-      // Reemplaza 'TU_CLAVE_DE_API' con tu propia clave de API de Google Maps
-      const apiKey = 'AIzaSyB3i1N6Nub2nrY8-03oOIu72rAUkclawF4';
+      const apiKey = "AIzaSyB3i1N6Nub2nrY8-03oOIu72rAUkclawF4"; // Reemplaza con tu propia clave de API de Google Maps
       
-      // Carga la API de Google Maps
       const script = document.createElement('script');
       script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`;
       script.defer = true;
       script.async = true;
 
-      // Callback que se ejecuta cuando se carga la API de Google Maps
       script.onload = () => {
-        // Lógica para inicializar el mapa o realizar otras operaciones
         const map = new window.google.maps.Map(document.getElementById('map'), {
           center: { lat: 28.6328, lng: -106.0691 }, // Coordenadas de Chihuahua
-          zoom: 12, // Zoom ajustado para ver la ciudad de Chihuahua adecuadamente
+          zoom: 12,
         });
 
-        // Puedes realizar más configuraciones y operaciones con el mapa según tus necesidades
+        // Coordenadas de los hoteles
+        const hotelCoordinates = [
+          { lat: 28.625952359070247, lng: -106.11916338185267 }, // Best Western Plus Chihuahua Juventud
+          { lat: 28.627265685921884, lng: -106.11945774857723 }, // Holiday Inn Express & Suites Chihuahua Juventud
+          { lat: 28.6634883437302, lng: -106.12814475158125 }, // Courtyard by Marriott Chihuahua
+          { lat: 28.658226472358354, lng: -106.12762330706971 }, // Hotel María Bonita Chihuahua
+          { lat: 28.63176041978389, lng: -106.0773653648619 }, // La Decima Guest House
+          { lat: 28.62032530696293, lng: -106.10922779665351 }, // Hotel Highland Chihuahua
+        ];
+
+        // Agregar marcadores para cada hotel
+        hotelCoordinates.forEach(({ lat, lng }) => {
+          new window.google.maps.Marker({
+            position: { lat, lng },
+            map,
+          });
+        });
       };
 
       document.head.appendChild(script);
     };
 
-    // Inicializa el mapa cuando el componente se monta
     initMap();
   }, []);
 
